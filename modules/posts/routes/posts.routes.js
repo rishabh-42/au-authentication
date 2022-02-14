d361756940681ffe9ab25c685bd45b849e8df296
Rishabh Rajput <rishabh.rajput@edfora.com>
2022-02-15 01:10:15 +0530

    Admin role check added in update and delete post api

--- modules/posts/routes/posts.routes.js
+++ modules/posts/routes/posts.routes.js
@@ -5,7 +5,7 @@ const { hasAdminRole } = require('../../../middlewares/authenticate');
 module.exports = (router) => {
     router.post('/auth/posts', hasAdminRole, validateAddPostData, addPost);
     router.get('/auth/posts', validateGetPostsData, getPosts);
-    router.delete('/auth/posts', validateDeletePostData, deletePost);
-    router.put('/auth/posts', validateUpdatePostData, updatePost);
+    router.delete('/auth/posts', hasAdminRole, validateDeletePostData, deletePost);
+    router.put('/auth/posts', hasAdminRole, validateUpdatePostData, updatePost);
 
 };
